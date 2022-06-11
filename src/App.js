bc1b01efd82b12813cc7a8684ab558db8b61be41
Lawrence Liu <lawrenceliu1212@gmail.com>
2022-06-10 23:53:00 -0400

    Added and testing toolbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './TopBar/topbar'
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
