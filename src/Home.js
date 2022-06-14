9d54937caba016eed47383b07eaf3f0b2ffd4107
Lawrence Liu <lawrence.liu.1@stonybrook.edu>
2022-06-14 01:08:38 -0400

    Started home screen

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './Home.css';
 import TopBar from './TopBar/topbar'
+import { Text } from "react-native";
 
 function Home() {
   return (
     <div className="Home">
       <TopBar />
+      <div className = "Home-Introduction">
+        Hello, my name is
+      </div>
+      <div className = "Introduction">
+        <Text numberOfLines={5}>
+          {'Lawrence Liu'}
+          {'It is nice to meet you!'}
+          </Text>
+      </div>
     </div>
   );
 }
